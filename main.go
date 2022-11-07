8e00ee5bd65a6f818e3298f50c2867bf51af2748
salmaqnsGH <nurussalamahqonaah@gmail.com>
2022-11-07 19:40:48 +0700

    Created user repository and created folder structure

--- main.go
+++ main.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/salmaqnsGH/crowdfunding-app/user"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
 	dsn := "host=localhost user=root password=secret dbname=crowdfunding port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("koneksi database berhasil!")
+
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "Test simpan",
+	}
+
+	userRepository.Save(user)
 }
